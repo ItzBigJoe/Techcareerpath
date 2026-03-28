b968d6e50f82991cc9746af4da55fe827d5dfd46
ItzBigJoe <abrahamjoseph3235@gmail.com>
2026-03-28 17:42:47 +0100

    Fix the problem with the download report

--- app/models/result.py
+++ app/models/result.py
@@ -2,7 +2,7 @@ from app.core.extensions import db
 
 class Result(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    session_id = db.Column(db.String(100))
+    user_id = db.Column(db.Integer)
     career = db.Column(db.String(100))
     score = db.Column(db.Integer)
     skills = db.Column(db.JSON)
